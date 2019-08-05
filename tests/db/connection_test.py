8f8ac77cd11f1bff68c797e037675e414e4c160a
Tersoo <codesage@live.com>
2019-08-05 03:05:14 -0400

    added tests for persistence to firestore run

---
+++ tests/db/connection_test.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+from firestore import Connection
+
+
+class TestConnection(TestCase):
+    
+    def setUp(self):
+        pass
+    
+    def tearDown(self):
+        pass
+    
+    def test_firebase_connection(self):
+        pass
